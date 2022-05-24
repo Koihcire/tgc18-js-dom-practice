b63973d3940206fccecf297c2d06366f0ed78872
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-05-24 17:52:08 +0800

    done q1 to q10

--- q6_counter/script.js
+++ q6_counter/script.js
@@ -4,6 +4,7 @@ document.querySelector('#increment').addEventListener('click', function () {
     if (num < 10) {
         document.querySelector('#box').innerText = num + 1;
     }
+    changeColor();
 })
 
 //q2 + q4
@@ -12,14 +13,23 @@ document.querySelector('#subtraction').addEventListener('click', function () {
     if (num > 0) {
         document.querySelector('#box').innerText = num - 1;
     }
+    changeColor();
 })
 
 //q3
 document.querySelector('#reset').addEventListener('click', function () {
     document.querySelector('#box').innerText = 0;
+    changeColor();
 })
 
-
+function changeColor(){
+    let num = parseInt(document.querySelector('#box').innerText);
+    if (num%2==0){
+        document.querySelector('#box').style.backgroundColor = 'green';
+    } else {
+        document.querySelector('#box').style.backgroundColor = 'red';
+    }
+}
 
 
 
