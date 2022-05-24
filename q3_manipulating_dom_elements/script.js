c41899e8e6136535d14fed88e1d0967f6ad62fa0
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-05-24 15:51:11 +0800

    up to q5

--- q3_manipulating_dom_elements/script.js
+++ q3_manipulating_dom_elements/script.js
@@ -1,6 +1,9 @@
 // PLACE YOUR CODE HERE
+document.querySelector('h1').innerText = 'Hello world!';
 
+document.querySelector('span#important').style.backgroundColor = 'red';
 
+document.querySelector('li.todo').style.font = 'verdana 16px'
 // LEAVE THE BELOW ALONE! DON'T TOUCH
 try {
    module.exports = exports = {header, important, todo};
---
+++ q4_more_manipulation/script.js
@@ -0,0 +1,18 @@
+//q1
+document.querySelector('h1').style.color = 'green';
+//q2
+document.querySelector('li.finished').innerText = 'Repay credit card debt';
+//q3
+document.querySelector('li#urgent').style.borderColor = 'black'
+document.querySelector('li#urgent').style.borderWidth = '2px'
+//q4
+document.querySelector('.emphasis').style.textDecoration = 'underline';
+//q5
+document.querySelector('p.emphasis').style.backgroundColor = 'orange';
+document.querySelector('p.emphasis').style.fontSize = '32px';
+document.querySelector('p.emphasis').style.lineHeight = '24px';
+//q6
+document.querySelector('li#urgent span.todo').style.backgroundColor = 'yellow';
+
+//q7
+document.querySelector('h2').innerHTML = '<span class="greetings">About Us</span>';
