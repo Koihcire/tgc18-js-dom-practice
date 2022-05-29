4982747f39a3e32064d2cfbd2673df525e061cc0
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-05-29 17:10:55 +0800

    r3q2

--- r3q2_Change_on_event/index.js
+++ r3q2_Change_on_event/index.js
@@ -1,3 +1,10 @@
-let box = document.getElementById('the-box');
 
-box.addEventListener(...)
+let box = document.getElementById("box");
+
+box.addEventListener("mouseover", function(){
+    box.style.backgroundColor = "black";
+})
+
+box.addEventListener("mouseout", function(){
+    box.style.backgroundColor = "white";
+})
